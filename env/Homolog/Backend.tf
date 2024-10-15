5cf6f1317943171de297e23c7f674df8809000d0
Arthur Amarante Muliterno <arthur_muliterno@hotmail.com>
2024-10-15 09:39:34 -0300

    Update Backend.tf

--- env/Homolog/Backend.tf
+++ env/Homolog/Backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "terraform-state-armuliterno"
-    key    = "Homolog/terraform.tfstate"
+    key    = "Prod/terraform.tfstate"
     region = "us-east-1"
   }
 }
